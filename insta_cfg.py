3c800a53e542bd8eb2d49298f6a00c78aa636a28
Sindhuja V <sindhuvsharma@gmail.com>
2017-12-17 14:08:57 +0000

    Heroku

--- insta_cfg.py
+++ insta_cfg.py
@@ -1,3 +1,3 @@
-DB_NAME = "instamanager"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}.db'.format(DB_NAME)
+import os
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SECRET_KEY = "e225eef6c3dadd7e1334b3b2c65d29dff60f2db374a7d4d7"
