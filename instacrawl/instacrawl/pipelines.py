d5ca39e702403c916210e00e1dd2947ae1cb9958
Sindhuja V <sindhuvsharma@gmail.com>
2017-12-30 15:08:36 +0000

    Crawler : user agent

--- instacrawl/instacrawl/pipelines.py
+++ instacrawl/instacrawl/pipelines.py
@@ -16,6 +16,7 @@ class InstacrawlPipeline(object):
             c.execute(stmt)
             self.conn.commit()
         except:
+            self.conn.rollback()
             raise
         finally:
             c.close()
